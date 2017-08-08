347970210796d3bea0990f2ff1c81884662aca07
Vaidotas Gedzius <vaidotas@gedzius.lt>
2017-08-08 22:31:25 +0100

    Added controls and initial code for them

--- sketch.js
+++ sketch.js
@@ -1,20 +1,55 @@
 var grid;
+var canvas;
 
-var nRows = 10;
-var nCols = 10;
-var w = 20;
+var nRows;
+var nCols;
+var w;
+
+var frameRate;
+
+// controls
+var rowsInput;
+var colsInput;
+var speedInput;
+var cellSizeInput;
+var generateBtn;
 
 function setup() {
   noStroke();
-  frameRate(100);
-  var canvas = createCanvas(nCols * w, nRows * w);
-  canvas.parent('canvas-container');
-
+  frameRate(frameRate);
+  noLoop();
   background(0);
 
+  rowsInput     = select('#size-x');
+  colsInput     = select('#size-y');
+  cellSizeInput = select('#cell-size');
+  speedInput    = select('#speed');
+  generateBtn   = select('#generate');
+
+  initVariables();
+
+  canvas = createCanvas(nCols * w, nRows * w);
+  canvas.parent('canvas-container');
+
   grid = new Grid(nCols, nRows);
+
+  generateBtn.mousePressed(function() {
+    initVariables();
+    loop();
+
+    canvas = resizeCanvas(nCols * w, nRows * w);
+
+    return false;
+  });
 }
 
 function draw() {
   grid.show().update();
+}
+
+function initVariables() {
+  nCols     = parseInt(colsInput.value());
+  nRows     = parseInt(rowsInput.value());
+  w         = parseInt(cellSizeInput.value());
+  frameRate = parseInt(speedInput.value());
 }
